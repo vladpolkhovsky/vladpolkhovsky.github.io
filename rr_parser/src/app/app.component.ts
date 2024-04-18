0b512743e105e24cf64bb989eb72ce284b0fdd21
vpolkhovsky <vlad.polkhovsky@gmail.com>
2024-04-18 16:34:29 +0300

    added rr_parser

--- rr_parser/src/app/app.component.ts
+++ rr_parser/src/app/app.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+
+export interface MetaColumn {
+  name: string,
+  type: string
+}
+
+export interface Table {
+  linesKeys: MetaColumn[],
+  values: string[]
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +17,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rr_parser';
+
+  @Input()
+  textAreaText: string = '';
+
+  tablesContent: {
+    key: string,
+    metaCols: MetaColumn[],
+    values: string[]
+  }[] = [];
+
+  processTextArea() {
+    var json = JSON.parse(this.textAreaText)
+    console.log(json)
+    for(let key in json) {
+      if (key.toLowerCase() == "transportguid") {
+        continue;
+      }
+      var index = this.tablesContent.push({
+        metaCols: [],
+        values: [],
+        key: key,
+      });
+      var escapedJson = JSON.parse(json[key]);
+      //var unescapedJson = JSON.parse(escapedJson);
+      console.log(escapedJson)
+      escapedJson.meta.forEach((metaEntry: MetaColumn) => {
+        this.tablesContent.at(index - 1)?.metaCols.push(metaEntry);
+      })
+      escapedJson.rows.forEach((value: string) => {
+        this.tablesContent.at(index - 1)?.values.push(value);
+      })
+    }
+    console.log(this.tablesContent)
+  }
 }
